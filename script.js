99f2b1c6577e9abd7f5698a1aa0802c7f8812c73
Vitor do Nascimento <45907317+vito0182@users.noreply.github.com>
2022-05-05 12:39:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var cont = 0;
-var text = String("olá cris, como vai? Bem, só queria te lembrar que ... gosto muito, muito, muito, muito, muitooo, de ... ... morango! hihi, e que gosto muitooo mais de você!!! mais do que dois milhões de morangos juntos! e olha que adoro morangos. mas sabe, também quero dizer que estou com saudades! quero te ver logo, aaaaa. adoro você meu moranguinho <3   ainda aqui? bem, um spoiler, estou fazendo alguns testes e estou com uma ideia de joguinho sobre o tarô de papel :p talvez dê certo, quem sabe :) e bom, se você continuou lendo... saiba também que ... amo você cris!");
+var text = String("your text goes here");
 var textAr = text.split(" ");
 
 var card2 = document.getElementById("card2");
